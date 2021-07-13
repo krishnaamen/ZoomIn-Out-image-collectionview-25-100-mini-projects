6c076c7b6c4876f8d40a8a8f4621138da0fef5a3
Krishna Prasad Khanal <krishnaamen@gmail.com>
2021-07-13 06:52:06 +0200

    First commit

---
+++ ZoomIn-Out-image-collectionview/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  ZoomIn-Out-image-collectionview
+//
+//  Created by Macbook on 12/07/2021.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet var img: UIImageView!
+    
+}
--- ZoomIn-Out-image-collectionview/ViewController.swift
+++ ZoomIn-Out-image-collectionview/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var collectionView: UICollectionView!
+    var imgArray = [#imageLiteral(resourceName: "a1"),#imageLiteral(resourceName: "a1"),#imageLiteral(resourceName: "a1"),#imageLiteral(resourceName: "a1"),#imageLiteral(resourceName: "a2"),#imageLiteral(resourceName: "a2"),#imageLiteral(resourceName: "a2"),#imageLiteral(resourceName: "a2"),#imageLiteral(resourceName: "a1"),#imageLiteral(resourceName: "a1"),#imageLiteral(resourceName: "a1"),#imageLiteral(resourceName: "a1"),#imageLiteral(resourceName: "a2"),#imageLiteral(resourceName: "a2"),#imageLiteral(resourceName: "a2"),#imageLiteral(resourceName: "a2"),#imageLiteral(resourceName: "a1"),#imageLiteral(resourceName: "a1"),#imageLiteral(resourceName: "a1"),#imageLiteral(resourceName: "a1"),#imageLiteral(resourceName: "a2"),#imageLiteral(resourceName: "a2"),#imageLiteral(resourceName: "a2"),#imageLiteral(resourceName: "a2")]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let imageInfo = GSImageInfo(image: imgArray[indexPath.row], imageMode: .aspectFit)
+        let transitionInfo = GSTransitionInfo(fromView: collectionView)
+        let imageViewer = GSImageViewerController(imageInfo: imageInfo, transitionInfo: transitionInfo)
+        imageViewer.dismissCompletion = {
+            print("dissmiss called")
+        }
+        present(imageViewer,animated: true)
     }
 
 
 }
 
+extension ViewController: UICollectionViewDelegate,UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+       return imgArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+        cell.img.layer.borderWidth = 1
+        cell.img.layer.masksToBounds = false
+        cell.img.layer.borderColor = UIColor.black.cgColor
+        cell.img.layer.cornerRadius = cell.img.frame.height
+        cell.img.clipsToBounds = true
+        cell.img.image = imgArray[indexPath.row]
+        return cell
+        
+    }
+    
+    
+}
